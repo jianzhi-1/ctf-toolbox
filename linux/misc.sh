c881343b53ac926d5a84b7f910eb26466f68069d
Jianzhi Wang <jianzhiw00@gmail.com>
2021-10-29 21:12:28 -0700

    Create misc.sh

---
+++ linux/misc.sh
@@ -0,0 +1,43 @@
+# change directory
+cd <dir_name>
+cd ..
+cd ~
+
+# make directory
+mkdir <dir_name>
+
+# print working directory
+pwd
+
+# list
+ls
+
+# list with hidden folders
+ls -al
+
+# read file
+cat <filename>
+
+# vim
+vim <filename>
+
+# remove file
+rm <filename>
+
+# remove directory
+rmdir <dirname>
+
+# remove directory, recursive and force
+rmdir -r <dirname>
+rmdir -rf <dirname>
+
+# rename
+mv <old_file_name> <new_file_name>
+
+# move
+mv <file_name> <dir_name>
+
+# echo environmental variable
+echo "$NAME"
+
+
